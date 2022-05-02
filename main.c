faa74f8f8f8cbeb4a0607a7d999d5ea9bd458606
Jin <jaehwanspin@gmail.com>
2022-05-03 01:18:50 +0900

    my head

--- main.c
+++ main.c
@@ -14,9 +14,11 @@
 
 /**
  * @author Jin
- * @brief concatenates two tokens
+ * @brief concatenates tokens
  */
-#define ___sentient_pp_cat(x, y) x ## y
+#define ___sentient_pp_cat(x, ...) x ## __VA_ARGS__
+#define ___sentient_pp_cat_3(x, y, ...) x ## y ## __VA_ARGS__
+#define ___sentient_pp_cat_4(x, y, ...) x ## y ## __VA_ARGS__
 
 /**
  * @author Jin
@@ -86,93 +88,32 @@
 #define ___sentient_pp_if_0_else(...)     __VA_ARGS__
 
 /**
- * @author Jin
- * @brief postpones an evaluation once
+ * @brief 
  * 
- * > #define ah() 1
- * >
- * > ___sentient_pp_defer(ah) () // postpones the macro
- * > ah empty() ()
- * > ah ()
- * > 1
- */
-#define ___sentient_pp_defer(x) x ___sentient_pp_empty()
-#define ___sentient_pp_defer_2(x) \
-		x ___sentient_pp_empty ___sentient_pp_empty()()
-#define ___sentient_pp_defer_3(x) \
-        x ___sentient_pp_empty ___sentient_pp_empty ___sentient_pp_empty()()()
-#define ___sentient_pp_defer_4(x) \
-        x ___sentient_pp_empty ___sentient_pp_empty ___sentient_pp_empty ___sentient_pp_empty()()()()
-#define ___sentient_pp_defer_5(x) \
-        x ___sentient_pp_empty ___sentient_pp_empty ___sentient_pp_empty ___sentient_pp_empty ___sentient_pp_empty()()()()
-#define ___sentient_pp_defer_6(x) \
-        x ___sentient_pp_empty ___sentient_pp_empty ___sentient_pp_empty ___sentient_pp_empty ___sentient_pp_empty ___sentient_pp_empty()()()()
-
-/**
- * @author Jin
- * @brief evaluates an expression immediately
- */
-#define ___sentient_pp_eval(...) ___sentient_pp_eval_128(__VA_ARGS__)
-#define ___sentient_pp_eval_128(...) ___sentient_pp_eval_64(__VA_ARGS__)
-#define ___sentient_pp_eval_64(...) ___sentient_pp_eval_32(__VA_ARGS__)
-#define ___sentient_pp_eval_32(...) ___sentient_pp_eval_16(__VA_ARGS__)
-#define ___sentient_pp_eval_16(...) ___sentient_pp_eval_8(__VA_ARGS__)
-#define ___sentient_pp_eval_8(...) ___sentient_pp_eval_4(__VA_ARGS__)
-#define ___sentient_pp_eval_4(...) ___sentient_pp_eval_2(__VA_ARGS__)
-#define ___sentient_pp_eval_2(...) ___sentient_pp_eval_1(__VA_ARGS__)
-#define ___sentient_pp_eval_1(...) __VA_ARGS__
-
-#define ___sentient_pp_terminate_args() 0
-
-#define ___sentient_pp_has_args(...) \
-		___sentient_pp_bool(___sentient_pp_first(___sentient_pp_terminate_args __VA_ARGS__)())
-
-/**
- * @author Jin
- * @brief repeats each macro arg
+ * count_args(4, 5, 6)
+ * count_args_impl(0, 4, 5, 6)
  * 
- * >
- * >
- * >
- * >
- * >
- * >
+ *                
+ * args(__VA_ARGS__, 6, 5, 4, 3, 2, 1, 0, )
+ * 
+ *       1  2  3  4  5  6  7  8
+ * args( 0, 4, 5, 6, 6, 5, 4, 3, 2, 1, 0, )
+ * 
+ * 3
+ * 
  */
-#define ___sentient_pp_map(macro, _arg1, ...)                         \
-		macro(_arg1)                                      \
-		___sentient_pp_if_else(___sentient_pp_has_args(__VA_ARGS__))              \
-		(                                                 \
-			___sentient_pp_defer_2(___sentient_pp_map_next)()(macro, __VA_ARGS__) \
-		)                                                 \
-		(                                                 \
-                                                          \
-		)
-		
-#define ___sentient_pp_map_next() ___sentient_pp_map
+#define args(_1, _2, _3, _4, _5, _6, _7, _8, ...) _8
+#define count_args(...) count_args_impl(0, __VA_ARGS__)
+#define count_args_impl(...) args(__VA_ARGS__, 6, 5, 4, 3, 2, 1, 0, )
 
-#define ___sentient_pp_is_sentient_impl 0
-
-/**
- * @author Jin
- * @brief defines a model struct with the type info
- */
-#define sentient_define_model(...) \
-		sentient_define_model_impl(__VA_ARGS__)
-
-#define sentient_define_model_impl(...)                                               \
-		___sentient_pp_if_else(___sentient_pp_cat(___sentient_pp_is_sentient_, impl)) \
-		(                                                                             \
-			sentient_define_model_impl_mode(__VA_ARGS__)                              \
-		)                                                                             \
-		(                                                                             \
-			sentient_define_model_decl_mode(__VA_ARGS__)                              \
-		)
-
-
+#include <sentient/core/internal/pp_iterations.h>
+#define ___sentient_pp_foreach(expr, ...) \
+		___sentient_pp_cat(___sentient_pp_foreach_, \
+					       ___sentient_pp_count_args(__VA_ARGS__)) (expr, __VA_ARGS__)
 
 int main(int argc, char** argv)
 {
-
+	const int num_args = ___sentient_pp_count_args(6, 7, 8);
 
 	return 0;
 }
