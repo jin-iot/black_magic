c6bf236d3259f8bfd56048be6655e513e1da4a5f
Jin <jaehwanspin@gmail.com>
2022-06-09 23:36:45 +0900

    mistake

--- include/sentient/core/internal/builtin_types.h
+++ include/sentient/core/internal/builtin_types.h
@@ -16,25 +16,26 @@ extern "C"
 
 #define ___SNT_PP_DECL_BUILTIN_TYPES() \
         ___SNT_PP_DECL_BUILTIN_TYPES_IMPL()
-#define ___SNT_PP_DECL_BUILTIN_TYPES_IMPL()      \
-        ___SNT_PP_FOREACH(                       \
-            ___SNT_PP_DECL_BUILTIN_TYPES_HANDLER \
-            (snt_u16, PRIMITIVE),                \
-            (snt_u32, PRIMITIVE),                \
-            (snt_u64, PRIMITIVE),                \
-            (snt_i8, PRIMITIVE),                 \
-            (snt_i16, PRIMITIVE),                \
-            (snt_i32, PRIMITIVE),                \
-            (snt_i64, PRIMITIVE),                \
-            (snt_f32, PRIMITIVE),                \
-            (snt_f64, PRIMITIVE),                \
-            (snt_str8, PRIMITIVE),               \
-            (snt_str16, PRIMITIVE),              \
-            (snt_str32, PRIMITIVE),              \
-            (snt_scdt32, STRUCTURE),             \
-            (snt_scdt64, STRUCTURE),             \
-            (snt_pt64, STRUCTURE),               \
-            (snt_ts128, STRUCTURE)               \
+#define ___SNT_PP_DECL_BUILTIN_TYPES_IMPL()       \
+        ___SNT_PP_FOREACH(                        \
+            ___SNT_PP_DECL_BUILTIN_TYPES_HANDLER, \
+            (snt_u8, PRIMITIVE),                  \
+            (snt_u16, PRIMITIVE),                 \
+            (snt_u32, PRIMITIVE),                 \
+            (snt_u64, PRIMITIVE),                 \
+            (snt_i8, PRIMITIVE),                  \
+            (snt_i16, PRIMITIVE),                 \
+            (snt_i32, PRIMITIVE),                 \
+            (snt_i64, PRIMITIVE),                 \
+            (snt_f32, PRIMITIVE),                 \
+            (snt_f64, PRIMITIVE),                 \
+            (snt_str8, PRIMITIVE),                \
+            (snt_str16, PRIMITIVE),               \
+            (snt_str32, PRIMITIVE),               \
+            (snt_scdt32, STRUCTURE),              \
+            (snt_scdt64, STRUCTURE),              \
+            (snt_pt64, STRUCTURE),                \
+            (snt_ts128, STRUCTURE)                \
         )
 
 
